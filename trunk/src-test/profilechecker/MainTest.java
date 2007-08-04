66f2798702e7f4990dc0c27bfd9f1a17d19642e7
matheusgr <matheusgr@415e2773-cb36-0410-bb12-b95cda405222>
2007-08-04 16:30:34 +0000

    ProfileCheker project infrastructure created.

---
+++ trunk/src-test/profilechecker/MainTest.java
@@ -0,0 +1,19 @@
+package profilechecker;
+
+import junit.framework.TestCase;
+
+/**
+ * TestCase of Main class.
+ * 
+ * @author Matheus
+ */
+public class MainTest extends TestCase {
+
+	/**
+	 * Calls the main at the main class.
+	 */
+	public void testMain() {
+		Main.main(null);
+	}
+	
+}
---
+++ trunk/src/profilechecker/Main.java
@@ -0,0 +1,20 @@
+package profilechecker;
+
+/**
+ * ProfileChecker main class.
+ * 
+ * @author Matheus
+ */
+public class Main {
+
+	/**
+	 * Method to be executed when invoking the ProfileChecker as an application.
+	 * 
+	 * @param args
+	 *            Arguments of the application.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Main-Class");
+	}
+
+}
