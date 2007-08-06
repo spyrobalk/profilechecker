2c4ba8dddb1495c6a2d912fad0545d9986de1588
matheusgr <matheusgr@415e2773-cb36-0410-bb12-b95cda405222>
2007-08-06 04:12:10 +0000

    Created XMIParserUI.

--- trunk/src/profilechecker/Main.java
+++ trunk/src/profilechecker/Main.java
@@ -1,8 +1,22 @@
 package profilechecker;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JFrame;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.xml.sax.SAXException;
 
 import profilechecker.parser.XMIParserGUI;
+import profilechecker.parser.XMIParserUI;
 
 /**
  * ProfileChecker main class.
@@ -11,6 +25,10 @@ import profilechecker.parser.XMIParserGUI;
  */
 public class Main {
 
+	private static final String PARSER_GUI_OPTION = "parsergui";
+	private static final String PARSER_UI_OPTION = "parserui";
+	private static final String HELP_OPTION = "help";
+
 	/**
 	 * Method to be executed when invoking the ProfileChecker as an application.
 	 * 
@@ -18,9 +36,53 @@ public class Main {
 	 *            Arguments of the application.
 	 */
 	public static void main(String[] args) {
-		XMIParserGUI xmiParserGUI = new XMIParserGUI();
-	    xmiParserGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    xmiParserGUI.setVisible(true);
+		Option parserGUIOption = new Option(PARSER_GUI_OPTION, "Starts XMIParserGUI");
+		Option parserUIOption = new Option(PARSER_UI_OPTION, true, "Starts XMIParserUI");
+		Option help = new Option(HELP_OPTION, "This help message");
+		
+		Options options = new Options();
+		
+		options.addOption(parserGUIOption);
+		options.addOption(parserUIOption);
+		options.addOption(help);
+
+	    // create the parser
+	    CommandLineParser parser = new GnuParser();
+	    try {
+	        // parse the command line arguments
+	        CommandLine line = parser.parse( options, args );
+	        if (line.hasOption(PARSER_UI_OPTION)) {
+	        	XMIParserUI xmiParserUI = new XMIParserUI();
+	        	File file = null;
+	        	try {
+	        		file = new File(line.getOptionValue(PARSER_UI_OPTION));
+					System.out.println(xmiParserUI.parse(file));
+				} catch (ParserConfigurationException e) {
+					System.err.println("Failed to parse file: " + file);
+					e.printStackTrace();
+				} catch (SAXException e) {
+					System.err.println("Failed to parse file: " + file);
+					e.printStackTrace();
+				} catch (IOException e) {
+					System.err.println("Failed to parse file: " + file);
+					e.printStackTrace();
+				}
+	        } else if (line.hasOption(PARSER_GUI_OPTION)) {
+	    		XMIParserGUI xmiParserGUI = new XMIParserGUI();
+	    	    xmiParserGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    	    xmiParserGUI.setVisible(true);
+	        } else {
+	        	// automatically generate the help statement
+	        	HelpFormatter formatter = new HelpFormatter();
+	        	formatter.printHelp( "java -jar profilechecker.jar", options );	        	
+	        }
+	    }
+	    catch( ParseException exp ) {
+	        // oops, something went wrong
+	        System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+	    }
+		
+
 	}
 
 }
