203214f6180ba3a476aea01a1f04acb1aff7e336
matheusgr <matheusgr@415e2773-cb36-0410-bb12-b95cda405222>
2007-08-20 14:24:27 +0000

    Parser changed to accept XMI names.

--- trunk/src-test/profilechecker/view/XMIParserGUITest.java
+++ trunk/src-test/profilechecker/view/XMIParserGUITest.java
@@ -31,7 +31,7 @@ public class XMIParserGUITest extends TestCase {
 				jep, jframe, controller, model);
 		acl.parseFile(new File(
 				"resources-test/profile_multi_stereotype_definition.xmi"));
-		String expectedResult = "<html><body>Profile<ul><li><b>name</b> public<li><b>id</b> _12_5_1_12e803d1_1186331641532_79382_142<li><b>visibility</b> publicV<p><li>Stereotype<ul><li><b>name</b> SimpleStereotype<li><b>id</b> _12_5_1_12e803d1_1186332374248_452459_298<li><b>visibility</b> publicV<li><b>type</b> UML Standard Profile::UML2.0 Metamodel::Classes::Kernel::Class<li><b>type</b> UML Standard Profile::UML2.0 Metamodel::Classes::Kernel::Association</ul></ul><hr /></body></html>";
+		String expectedResult = "<html><body>Profile<ul><li><b>name</b> public<li><b>id</b> _12_5_1_12e803d1_1186331641532_79382_142<li><b>visibility</b> publicV<p><li>Stereotype<ul><li><b>name</b> SimpleStereotype<li><b>id</b> _12_5_1_12e803d1_1186332374248_452459_298<li><b>visibility</b> publicV<li><b>type</b> Class<li><b>type</b> Association</ul></ul><hr /></body></html>";
 		assertEquals(expectedResult, jep.getText());
 	}
 
