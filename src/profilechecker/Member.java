bfec726cf0a3cc28a2008c5db4328fc71dcd8154
matheusgr <matheusgr@415e2773-cb36-0410-bb12-b95cda405222>
2007-08-20 06:50:31 +0000

    xmiType renamed to type.

--- src/profilechecker/Member.java
+++ src/profilechecker/Member.java
@@ -3,16 +3,34 @@ package profilechecker;
 public class Member extends OwnedMember{
 	
 	private String id;
-	private String xmiType;
+	private String type;
 	
 	public Member(){
 		
 	}
 	
-	public Member(String name, String id, VisibilityType visibility, String xmiType){
+	public Member(String name, String id, VisibilityType visibility, String type){
 		super(name,id,visibility);
 		this.id = id;
-		this.xmiType = xmiType;
+		this.type = type;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 	
 	
